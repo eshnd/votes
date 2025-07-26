9e217da36b4aa382c92bb5d2247cd5ac2a9ebf68
eshon <eshaan2031@icloud.com>
2025-07-26 10:16:04 -0500

    this works

---
+++ main.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+name = sys.argv[1]
+url = f'https://aur.archlinux.org/packages/{name}'  
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+tags = soup.find_all(['th', 'td'])
+data = [tag.get_text(strip=True) for tag in tags]
+
+for i in range(len(data)):
+    if (data[i] == "Votes:"):
+        print("Votes: " + data[i + 1])
+        sys.exit()
+print(f'No package found with name: "{name}"')
